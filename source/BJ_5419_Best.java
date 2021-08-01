555a6a7189d304fb824cef2fea29550f01d8324e
KDY <horsefood@gmail.com>
2021-08-01 23:15:19 +0900

    BJ 5419

--- source/BJ_5419_Best.java
+++ source/BJ_5419_Best.java
@@ -1,15 +1,31 @@
 package swPro.source;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+/*
+ * list : 섬의 정보를 저장(x,y좌표 : [-10^9 : 10^9])
+ * 		- 탐색을 위해 ArrayList사용
+ * xPos : 좌표압축을 위한 리스트, x좌표만 저장
+ * 		- 데이터 삭제를 위해 LinkedList사용
+ * map : 좌표압축을 위한 hashmap, 중복제거 및 키값으로 탐색
+ * 
+ * 문제 풀이
+ * 1. 섬의 좌표를 입력받고, x좌표를 기준으로 좌표압축 (섬은 최대 75000개)
+ * 2. 섬의 y좌표 기준 내림차순정렬, y같으면 x기준 오름차순 정렬
+ * 		- y좌표가 증가하는 방향은 북쪽, x좌표가 증가하는 방향은 동쪽
+ * y
+ * |-------------------------------
+ * | (-10,10)		(10,10)         
+ * | 
+ * | (-10,-10)		(10,-10)
+ * |-------------------------------x
+ * 정렬 후: (-10,10) (10,10) (-10,-10) (10,-10)
+ * 	압축 좌표:   1          2          1           2 
+ * 3. 인덱스트리를 이용하여 x좌표 기준으로 더 작거나 같은 값 탐색 (자신에게 올 수 있는 섬들을 카운트)
+ * 		(-10,10) (10,10) (-10,-10) (10,-10)
+ * 		0 -> 1 -> 1 -> 3 = 총 5개의 쌍
+ */
 public class BJ_5419_Best {
 	static int leaf;
 	static int[] tree;
@@ -24,6 +40,7 @@ public class BJ_5419_Best {
 			int N = Integer.parseInt(br.readLine().trim());
 			
 			ArrayList<Node> list = new ArrayList<>();
+			LinkedList<Integer> xPos = new LinkedList<>();
 			
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine().trim());
@@ -32,32 +49,29 @@ public class BJ_5419_Best {
 				int y = Integer.parseInt(st.nextToken());
 				
 				list.add(new Node (x,y));
+				xPos.add(x);
+			}
+			// x좌표 기준 좌표압축
+			Collections.sort(xPos);
+			
+			HashMap<Integer, Integer> map = new HashMap<>();
+			int idx = 0;
+			
+			while(!xPos.isEmpty()) {
+				int getX = xPos.poll();
+				if(!map.containsKey(getX)) map.put(getX, ++idx);
 			}
 			
-			Collections.sort(list, new Comparator<Node>() {
-				@Override
-				public int compare(Node o1, Node o2) {
-					// x기준 오름차순 정렬
-					return Integer.compare(o1.x, o2.x);
-				}
-			});
-			
-			int idx = 1;
-			list.get(0).idx = idx;
-			for(int i=1; i<list.size(); i++) {
-				if(list.get(i).x == list.get(i-1).x) list.get(i).idx = idx;
-				else {
-					list.get(i).idx = ++idx;
-				}
-			}
+			// 섬의 좌표 정렬
 			Collections.sort(list);
 			
 			init(idx);
 			
 			long cnt = 0;
 			for(Node island: list) {
-				cnt += query(island.idx);
-				update(island.idx);
+				int key = map.get(island.x);
+				cnt += query(key);
+				update(key);
 			}
 			
 			bw.write(cnt+"\n");
@@ -68,7 +82,7 @@ public class BJ_5419_Best {
 	
 	static void init(int idx) {
 		leaf = 1;
-		
+
 		while(leaf < idx) {
 			leaf *= 2;
 		}
@@ -107,12 +121,10 @@ public class BJ_5419_Best {
 	static class Node implements Comparable<Node> {
 		int x;
 		int y;
-		int idx;
 		
 		public Node(int x, int y) {
 			this.x = x;
 			this.y = y;
-			this.idx = 0;
 		}
 
 		@Override
